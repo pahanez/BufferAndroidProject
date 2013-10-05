c7f0bfd46c1f25e94e5c5ca7ed1f9b3b61ff6ef7
pahanez <pahanez@gmail.com>
2013-10-05 20:04:22 +0300

    initial fragmenttemplate

---
+++ src/com/example/fragmenttemplate/CustomFragment.java
@@ -0,0 +1,16 @@
+package com.example.fragmenttemplate;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.actionbarsherlock.app.SherlockFragment;
+
+public class CustomFragment extends SherlockFragment{
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		return inflater.inflate(R.layout.fragment_activity, null, false);
+	}
+}
---
+++ src/com/example/fragmenttemplate/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.fragmenttemplate;
+
+import android.os.Bundle;
+
+import com.actionbarsherlock.app.SherlockFragmentActivity;
+
+public class MainActivity extends SherlockFragmentActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		getSupportFragmentManager().beginTransaction().add(R.id.fragment_holder, new CustomFragment()).commit();
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(com.actionbarsherlock.view.Menu menu) {
+		getSupportMenuInflater().inflate(R.menu.main, menu);
+		
+		return true;
+	}
+
+}
