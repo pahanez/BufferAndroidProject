9403679814bfaca32880299aaef17f3419c62a41
pahanez <pahanez@gmail.com>
2013-10-06 22:21:49 +0300

    single type adapter

--- src/com/example/fragmenttemplate/CstmFrgImpl.java
+++ src/com/example/fragmenttemplate/CstmFrgImpl.java
@@ -18,33 +18,30 @@ public class CstmFrgImpl extends AListFragment<Device> {
 	public void onActivityCreated(Bundle savedInstanceState) {
 		super.onActivityCreated(savedInstanceState);
 		setEmptyText("No results found");
-		
-		/*new Handler(getActivity().getMainLooper()){
-			public void handleMessage(android.os.Message msg) {
-				getActivity().runOnUiThread(new Runnable() {
-					
-					@Override
-					public void run() {
-						refreshWithProgress();
-					}
-				});
-			};
-		}.sendEmptyMessageDelayed(1, 5000);*/
+
+		/*
+		 * new Handler(getActivity().getMainLooper()){ public void
+		 * handleMessage(android.os.Message msg) {
+		 * getActivity().runOnUiThread(new Runnable() {
+		 * 
+		 * @Override public void run() { refreshWithProgress(); } }); };
+		 * }.sendEmptyMessageDelayed(1, 5000);
+		 */
 	}
 
 	@Override
 	protected SingleTypeAdapter<Device> createAdapter(List<Device> items) {
-		return new SingleTypeAdapter<Device>(getActivity(),
-				R.layout.device_row) {
+		return new SingleTypeAdapter<Device>(getActivity(), R.layout.device_row) {
 
 			@Override
 			protected int[] getChildViewIds() {
 				return new int[] { R.id.device_iv, R.id.device_tv };
 			}
+
 			@Override
 			protected void update(int position, Device item) {
 				imageView(0).setImageResource(item.img);
-				setText(1 , item.name);
+				setText(1, item.name);
 			}
 		};
 	}
@@ -53,15 +50,16 @@ public class CstmFrgImpl extends AListFragment<Device> {
 	protected int getErrorMessage(Exception exception) {
 		return R.string.error_custom_load;
 	}
-	
+
 	@Override
 	public void onViewCreated(View view, Bundle savedInstanceState) {
 		super.onViewCreated(view, savedInstanceState);
 	}
-	@Override
+
+	@Override  
 	public Loader<List<Device>> onCreateLoader(int arg0, Bundle arg1) {
 
-		return new CustomLoader<List<Device>>(getActivity()){
+		return new CustomLoader<List<Device>>(getActivity()) {
 			@Override
 			public List<Device> loadInBackground() {
 				try {
@@ -71,12 +69,13 @@ public class CstmFrgImpl extends AListFragment<Device> {
 				}
 				List<Device> devices = new ArrayList<Device>();
 				for (int i = 0; i < 100; i++) {
-					devices.add(new Device(i, "Device #" + i, R.drawable.device_icon));
+					devices.add(new Device(i, "Device #" + i,
+							R.drawable.device_icon));
 				}
-				
+
 				return devices;
 			}
 		};
 	}
-	
+
 }
