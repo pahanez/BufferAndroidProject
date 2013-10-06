442807a287a9dcce3a69770ab7e59cbaa67038cb
pahanez <pahanez@gmail.com>
2013-10-06 03:00:06 +0300

    ItemListFrgmnt

--- src/com/example/fragmenttemplate/ItemListFrgmnt.java
+++ src/com/example/fragmenttemplate/ItemListFrgmnt.java
@@ -3,20 +3,28 @@ package com.example.fragmenttemplate;
 import java.util.Collections;
 import java.util.List;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.app.LoaderManager.LoaderCallbacks;
 import android.support.v4.content.Loader;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.animation.AnimationUtils;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ListView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.github.kevinsawicki.wishlist.SingleTypeAdapter;
 import com.github.kevinsawicki.wishlist.ViewUtils;
-import com.github.mobile.ui.ItemListFragment;
 
-public class ItemListFrgmnt<E> extends CustomFragmentItem implements
-		LoaderCallbacks<String> { // TODO <String>
+public abstract class ItemListFrgmnt<E> extends CustomFragmentItem implements
+		LoaderCallbacks<List<E>> { // TODO <String>
+
+	private static final String FORCE_REFRESH = "forceRefresh";
 
 	/**
 	 * List items provided to {@link #onLoadFinished(Loader, List)}
@@ -27,146 +35,333 @@ public class ItemListFrgmnt<E> extends CustomFragmentItem implements
 	 * List view
 	 */
 	protected ListView listView;
-	
+
 	/**
-     * Empty view
-     */
-    protected TextView emptyView;
+	 * Empty view
+	 */
+	protected TextView emptyView;
 
-    /**
-     * Progress bar
-     */
-    protected ProgressBar progressBar;
+	/**
+	 * Progress bar
+	 */
+	protected ProgressBar progressBar;
 
-    /**
-     * Is the list currently shown?
-     */
-    protected boolean listShown;
+	/**
+	 * Is the list currently shown?
+	 */
+	protected boolean listShown;
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+
+		return inflater.inflate(R.layout.item_list, null);
+	}
 
 	@Override
 	public void onViewCreated(View view, Bundle savedInstanceState) {
 		super.onViewCreated(view, savedInstanceState);
+		listView = mViewFinder.find(android.R.id.list);
+		listView.setOnItemClickListener(new OnItemClickListener() {
+
+			@Override
+			public void onItemClick(AdapterView<?> parent, View view,
+					int position, long id) {
+				onListItemClick((ListView) parent, view, position, id);
+			}
+		});
+		progressBar = mViewFinder.find(R.id.pb_loading);
+
+		emptyView = mViewFinder.find(android.R.id.empty);
+
+		configureList(getActivity(), getListView());
+	}
+
+	@Override
+	public void onActivityCreated(Bundle savedInstanceState) {
+		super.onActivityCreated(savedInstanceState);
 		if (!items.isEmpty())
 			setListShown(true, false);
 	}
-	
+
+	@Override
+	public void onDestroyView() {
+		listShown = false;
+		emptyView = null;
+		progressBar = null;
+		listView = null;
+
+		super.onDestroyView();
+	}
+
 	/**
-     * Set list shown or progress bar show
-     *
-     * @param shown
-     * @return this fragment
-     */
-    public ItemListFrgmnt<E> setListShown(final boolean shown) {
-        return setListShown(shown, true);
-    }
+	 * Set the list to be shown
+	 */
+	protected void showList() {
+		setListShown(true, isResumed());
+	}
+
+	/**
+	 * Set list shown or progress bar show
+	 * 
+	 * @param shown
+	 * @return this fragment
+	 */
+	public ItemListFrgmnt<E> setListShown(final boolean shown) {
+		return setListShown(shown, true);
+	}
 
 	private ItemListFrgmnt<E> setListShown(boolean shown, boolean animate) {
 
-        if (!isUsable())
-            return this;
+		if (!isUsable())
+			return this;
 
-        if (shown == listShown) {
-            if (shown)
-                // List has already been shown so hide/show the empty view with
-                // no fade effect
-                if (items.isEmpty())
-                    hide(listView).show(emptyView);
-                else
-                    hide(emptyView).show(listView);
-            return this;
-        }
+		if (shown == listShown) {
+			if (shown)
+				// List has already been shown so hide/show the empty view with
+				// no fade effect
+				if (items.isEmpty())
+					hide(listView).show(emptyView);
+				else
+					hide(emptyView).show(listView);
+			return this;
+		}
 
-        listShown = shown;
+		listShown = shown;
 
-        if (shown)
-            if (!items.isEmpty())
-                hide(progressBar).hide(emptyView).fadeIn(listView, animate)
-                        .show(listView);
-            else
-                hide(progressBar).hide(listView).fadeIn(emptyView, animate)
-                        .show(emptyView);
-        else
-            hide(listView).hide(emptyView).fadeIn(progressBar, animate)
-                    .show(progressBar);
+		if (shown)
+			if (!items.isEmpty())
+				hide(progressBar).hide(emptyView).fadeIn(listView, animate)
+						.show(listView);
+			else
+				hide(progressBar).hide(listView).fadeIn(emptyView, animate)
+						.show(emptyView);
+		else
+			hide(listView).hide(emptyView).fadeIn(progressBar, animate)
+					.show(progressBar);
 
-        return this;
-    
+		return this;
+
 	}
-	
-	
-	
+
 	private ItemListFrgmnt<E> fadeIn(final View view, final boolean animate) {
-        if (view != null)
-            if (animate)
-                view.startAnimation(AnimationUtils.loadAnimation(getActivity(),
-                        android.R.anim.fade_in));
-            else
-                view.clearAnimation();
-        return this;
-    }
+		if (view != null)
+			if (animate)
+				view.startAnimation(AnimationUtils.loadAnimation(getActivity(),
+						android.R.anim.fade_in));
+			else
+				view.clearAnimation();
+		return this;
+	}
 
-    private ItemListFrgmnt<E> show(final View view) {
-        ViewUtils.setGone(view, false);
-        return this;
-    }
+	private ItemListFrgmnt<E> show(final View view) {
+		ViewUtils.setGone(view, false);
+		return this;
+	}
 
-    private ItemListFrgmnt<E> hide(final View view) {
-        ViewUtils.setGone(view, true);
-        return this;
-    }
-    
-    /**
-     * Set empty text on list fragment
-     *
-     * @param message
-     * @return this fragment
-     */
-    protected ItemListFrgmnt<E> setEmptyText(final String message) {
-        if (emptyView != null)
-            emptyView.setText(message);
-        return this;
-    }
-    
-    /**
-     * Set empty text on list fragment
-     *
-     * @param resId
-     * @return this fragment
-     */
-    protected ItemListFrgmnt<E> setEmptyText(final int resId) {
-        if (emptyView != null)
-            emptyView.setText(resId);
-        return this;
-    }
-    
-    /**
-     * Callback when a list view item is clicked
-     *
-     * @param l
-     * @param v
-     * @param position
-     * @param id
-     */
-    public void onListItemClick(ListView l, View v, int position, long id) {
-    }
-	
-	
+	private ItemListFrgmnt<E> hide(final View view) {
+		ViewUtils.setGone(view, true);
+		return this;
+	}
+
+	/**
+	 * Set empty text on list fragment
+	 * 
+	 * @param message
+	 * @return this fragment
+	 */
+	protected ItemListFrgmnt<E> setEmptyText(final String message) {
+		if (emptyView != null)
+			emptyView.setText(message);
+		return this;
+	}
+
+	/**
+	 * Set empty text on list fragment
+	 * 
+	 * @param resId
+	 * @return this fragment
+	 */
+	protected ItemListFrgmnt<E> setEmptyText(final int resId) {
+		if (emptyView != null)
+			emptyView.setText(resId);
+		return this;
+	}
+
+	/**
+	 * Force a refresh of the items displayed ignoring any cached items
+	 */
+	protected void forceRefresh() {
+		Bundle bundle = new Bundle();
+		bundle.putBoolean(FORCE_REFRESH, true);
+		refresh(bundle);
+	}
+
+	/**
+	 * Refresh the list with the progress bar showing
+	 */
+	protected void refreshWithProgress() {
+		items.clear();
+		setListShown(false);
+		refresh();
+	}
+
+	/**
+	 * Refresh the fragment's list
+	 */
+	public void refresh() {
+		refresh(null);
+	}
+
+	private void refresh(final Bundle args) {
+		if (!isUsable())
+			return;
+		getSherlockActivity()
+				.setSupportProgressBarIndeterminateVisibility(true);
+		getLoaderManager().restartLoader(0, args, this); // TODO
+	}
+
+	/**
+	 * Get {@link ListView}
+	 * 
+	 * @return listView
+	 */
+	public ListView getListView() {
+		return listView;
+	}
+
+	/**
+	 * Configure list after view has been created
+	 * 
+	 * @param activity
+	 * @param listView
+	 */
+	protected void configureList(Activity activity, ListView listView) {
+		// listView.setAdapter(createAdapter()); //TODO
+	}
+
+	/**
+	 * Get list adapter
+	 * 
+	 * @return list adapter
+	 */
+	@SuppressWarnings("unchecked")
+	protected HeaderFooterListAdapter<SingleTypeAdapter<E>> getListAdapter() {
+		if (listView != null)
+			return (HeaderFooterListAdapter<SingleTypeAdapter<E>>) listView
+					.getAdapter();
+		else
+			return null;
+	}
+
+	/**
+	 * Notify the underlying adapter that the data set has changed
+	 * 
+	 * @return this fragment
+	 */
+	protected ItemListFrgmnt<E> notifyDataSetChanged() {
+		HeaderFooterListAdapter<SingleTypeAdapter<E>> root = getListAdapter();
+		if (root != null) {
+			SingleTypeAdapter<E> typeAdapter = root.getWrappedAdapter();
+			if (typeAdapter != null)
+				typeAdapter.notifyDataSetChanged();
+		}
+		return this;
+	}
+
+	/**
+	 * Create adapter to display items
+	 * 
+	 * @return adapter
+	 */
+
+	protected HeaderFooterListAdapter<SingleTypeAdapter<E>> createAdapter() {
+		SingleTypeAdapter<E> wrapped = createAdapter(items);
+		return new HeaderFooterListAdapter<SingleTypeAdapter<E>>(getListView(),
+				wrapped);
+	}
+
+	/**
+	 * Create adapter to display items
+	 * 
+	 * @param items
+	 * @return adapter
+	 */
+	protected abstract SingleTypeAdapter<E> createAdapter(final List<E> items);
+
+	/**
+	 * Callback when a list view item is clicked
+	 * 
+	 * @param l
+	 * @param v
+	 * @param position
+	 * @param id
+	 */
+	public void onListItemClick(ListView l, View v, int position, long id) {
+	}
 
 	@Override
-	public Loader<String> onCreateLoader(int arg0, Bundle arg1) {
-		// TODO Auto-generated method stub
+	public void onLoadFinished(Loader<List<E>> loader, List<E> items) {
+
+		if (!isUsable())
+			return;
+
+		getSherlockActivity().setSupportProgressBarIndeterminateVisibility(
+				false);
+		Exception exception = getException(loader); // TODO
+		if (exception != null) {
+			showError(exception, getErrorMessage(exception));
+			showList();
+			return;
+		}
+
+		this.items = items;
+		getListAdapter().getWrappedAdapter().setItems(items.toArray());
+		showList();
+
+	}
+
+	/**
+	 * Show exception in a {@link Toast}
+	 * 
+	 * @param e
+	 * @param defaultMessage
+	 */
+	protected void showError(final Exception e, final int defaultMessage) {
+		Toasts.show(getActivity(), e, defaultMessage);
+	}
+
+	/**
+	 * Get error message to display for exception
+	 * 
+	 * @param exception
+	 * @return string resource id
+	 */
+	protected abstract int getErrorMessage(Exception exception);
+
+	/**
+	 * Get exception from loader if it provides one by being a
+	 * {@link ThrowableLoader}
+	 * 
+	 * @param loader
+	 * @return exception or null if none provided
+	 */
+	protected Exception getException(final Loader<List<E>> loader) {
+		// if (loader instanceof ThrowableLoader)
+		// return ((ThrowableLoader<List<E>>) loader).clearException();
+		// else
 		return null;
 	}
 
 	@Override
-	public void onLoadFinished(Loader<String> arg0, String arg1) {
-		// TODO Auto-generated method stub
-
+	public Loader<List<E>> onCreateLoader(int arg0, Bundle arg1) {
+		// TODO
+		return null;
 	}
 
 	@Override
-	public void onLoaderReset(Loader<String> arg0) {
-		// TODO Auto-generated method stub
-
+	public void onLoaderReset(Loader<List<E>> arg0) {
+		// Intentionally left blank
 	}
+
 }
