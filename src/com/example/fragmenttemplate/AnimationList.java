abfa73ca94a7b955ad613c529ae0938db5ca6e9f
pahanez <pahanez@gmail.com>
2013-10-16 00:47:46 +0300

    Animation R&D

--- src/com/example/fragmenttemplate/AnimationList.java
+++ src/com/example/fragmenttemplate/AnimationList.java
@@ -1,19 +1,33 @@
 package com.example.fragmenttemplate;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.Animation.AnimationListener;
+import android.view.animation.AnimationUtils;
+import android.view.animation.LayoutAnimationController;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.actionbarsherlock.app.SherlockListFragment;
 import com.example.fragmenttemplate.utils.ViewFinder;
 
-public class AnimationList extends SherlockListFragment {
+public class AnimationList extends SherlockListFragment implements OnItemClickListener {
 	
 	private ViewFinder mViewFinder;
 	private ListView mListView;
+	private ArrayAdapter<String> mArrayAdapter;
+	private List<String> data;
+	private int deletedPosition;
 	
 	public android.view.View onCreateView(LayoutInflater inflater,
 			ViewGroup container, Bundle savedInstanceState) {
@@ -26,11 +40,60 @@ public class AnimationList extends SherlockListFragment {
 		mViewFinder = new ViewFinder(view);
 		
 		mListView = mViewFinder.find(android.R.id.list);
-		String[] data = new String[100];
-		for (int i = 0; i < data.length; i++) {
-			data[i] = "data " + i;
+		data = new ArrayList<String>();
+		for (int i = 0; i < 100; i++) {
+			data.add("data " + i); 
 		}
 		
-		mListView.setAdapter(new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, data ));
+		
+		mArrayAdapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, data );
+			
+		mListView.setAdapter(mArrayAdapter);
+		
+		
+		
+		 final Animation animation = AnimationUtils.loadAnimation(getActivity(),
+		            R.anim.fdas);
+		    animation.setAnimationListener(new AnimationListener() {
+		        @Override
+		        public void onAnimationStart(Animation animation) {
+		        }
+
+		        @Override
+		        public void onAnimationRepeat(Animation animation) {
+		        }
+
+		        @Override
+		        public void onAnimationEnd(Animation animation) {
+		            mArrayAdapter.remove(mArrayAdapter.getItem(deletedPosition));
+		            mArrayAdapter.notifyDataSetChanged();
+		        }
+		    });
+		    
+		    mListView.setOnItemClickListener(new OnItemClickListener() {
+		        @Override
+		        public void onItemClick(AdapterView<?> parent, View view,
+		                int position, long id) {
+		        	deletedPosition = position;
+		            view.startAnimation(animation);
+		        }
+
+		    });
 	}
+
+	private void initAnimation() {
+		Animation animation = new AlphaAnimation(1.0f, 0.0f);
+		animation.setDuration(500);
+		LayoutAnimationController lac = new LayoutAnimationController(animation);
+		mListView.setLayoutAnimation(lac);
+	}
+
+	@Override
+	public void onItemClick(AdapterView<?> parent, View view, int position,
+			long id) { 
+		
+	}
+	
+	
+	
 }
