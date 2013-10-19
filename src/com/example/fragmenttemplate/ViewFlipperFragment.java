2d8dc27f5cd509b9f1946b34206715257dc7488d
pahanez <pahanez@gmail.com>
2013-10-19 19:37:07 +0300

    [R&D]

--- src/com/example/fragmenttemplate/ViewFlipperFragment.java
+++ src/com/example/fragmenttemplate/ViewFlipperFragment.java
@@ -5,8 +5,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.ViewFlipper;
@@ -16,7 +14,7 @@ import com.example.fragmenttemplate.utils.ViewFinder;
 
 public class ViewFlipperFragment extends SherlockFragment implements OnClickListener{
 	
-	private ViewFlipper mViewFlipper;
+	private ViewFlipper mViewFlipper; 
 	private Button b1,b2;
 	
 	@Override
