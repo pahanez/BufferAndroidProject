ca16b0bf26ac8d0a03c607415ac64e01240b12d7
pahanez <pahanez@gmail.com>
2013-11-01 00:59:12 +0300

    Ubuntu one android app investigation

---
+++ src/com/example/fragmenttemplate/ApplicationClass.java
@@ -0,0 +1,33 @@
+package com.example.fragmenttemplate;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import android.app.Application;
+
+public class ApplicationClass extends Application{
+	
+	/**
+	 * From GreenDroid
+	 * */
+    private static final int CORE_POOL_SIZE = 5;
+
+    private static final ThreadFactory sThreadFactory = new ThreadFactory() {
+        private final AtomicInteger mCount = new AtomicInteger(1);
+
+        public Thread newThread(Runnable r) {
+            return new Thread(r, "Thread #" + mCount.getAndIncrement());
+        }
+    };
+    
+    public ExecutorService getExecutor() {
+        if (mExecutorService == null) {
+            mExecutorService = Executors.newFixedThreadPool(CORE_POOL_SIZE, sThreadFactory);
+        }
+        return mExecutorService;
+    }
+
+    private ExecutorService mExecutorService;
+}
