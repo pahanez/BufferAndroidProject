32b25e3304181932da9d3f505f4d9603462fca55
pahanez <pahanez@gmail.com>
2013-11-04 23:13:52 +0300

    R&D

---
+++ src/com/example/fragmenttemplate/view/LayoutPhase.java
@@ -0,0 +1,24 @@
+package com.example.fragmenttemplate.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class LayoutPhase extends View {
+
+	public LayoutPhase(Context context, AttributeSet attrs) {
+		super(context, attrs);
+	}
+	
+	@Override
+	protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+		super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+	}
+	
+	@Override
+	protected void onLayout(boolean changed, int left, int top, int right,
+			int bottom) {
+		super.onLayout(changed, left, top, right, bottom);
+	}
+
+}
