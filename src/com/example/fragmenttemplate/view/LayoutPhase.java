d641cb8ef52b614b1977fc23f93ef9d5e2c799cf
pahanez <pahanez@gmail.com>
2013-11-08 01:10:54 +0300

    R&D

--- src/com/example/fragmenttemplate/view/LayoutPhase.java
+++ src/com/example/fragmenttemplate/view/LayoutPhase.java
@@ -1,6 +1,7 @@
 package com.example.fragmenttemplate.view;
 
 import android.content.Context;
+import android.graphics.Canvas;
 import android.util.AttributeSet;
 import android.view.View;
 
@@ -19,6 +20,11 @@ public class LayoutPhase extends View {
 	protected void onLayout(boolean changed, int left, int top, int right,
 			int bottom) {
 		super.onLayout(changed, left, top, right, bottom);
+	} 
+	
+	@Override
+	protected void dispatchDraw(Canvas canvas) {
+		super.dispatchDraw(canvas);
 	}
 
 }
