3c9f6de893722ce80e7b914b3178febd8c077d39
pahanez <pahanez@gmail.com>
2013-11-19 23:04:30 +0300

    R&D

--- src/com/example/fragmenttemplate/MainActivity.java
+++ src/com/example/fragmenttemplate/MainActivity.java
@@ -52,4 +52,5 @@ public class MainActivity extends SherlockFragmentActivity implements LoaderCall
 //01.11.2013 git@github.com:casidiablo/groundy.git
 //13.11.2013 Ubuntu One GaleryActivity
 //14.11.2013 Ubuntu One Login Process
+//19.11.2013 Ubuntu One
 }
