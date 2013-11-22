a2457e5c01c5fef9d6a1f2317969eaed5948d0b9
pahanez <pahanez@gmail.com>
2013-11-23 00:59:58 +0300

    HackerTyper Demo

--- src/com/example/fragmenttemplate/HackerTyperFragment.java
+++ src/com/example/fragmenttemplate/HackerTyperFragment.java
@@ -1,16 +1,35 @@
 package com.example.fragmenttemplate;
 
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnKeyListener;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 import com.actionbarsherlock.app.SherlockFragment;
 
-public class HackerTyperFragment extends SherlockFragment{
+public class HackerTyperFragment extends SherlockFragment implements OnKeyListener{
+	
+	private EditText mTextView;
+	
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 		return inflater.inflate(R.layout.hacker_typer_layout, null);
+	}
+	
+	@Override
+	public void onViewCreated(View view, Bundle savedInstanceState) {
+		super.onViewCreated(view, savedInstanceState);
+		mTextView = (EditText) view.findViewById(R.id.hacker_typer_et);
+		mTextView.setOnKeyListener(this);
+	}
+
+	@Override
+	public boolean onKey(View v, int keyCode, KeyEvent event) {
+		
+		return false;
 	}
 }
