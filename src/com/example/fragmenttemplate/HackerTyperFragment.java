63eaac367f4f597d1d5477d7bbc0c9afa463c484
pahanez <pahanez@gmail.com>
2013-11-24 15:35:45 +0300

    HackerTyper demo

--- src/com/example/fragmenttemplate/HackerTyperFragment.java
+++ src/com/example/fragmenttemplate/HackerTyperFragment.java
@@ -7,9 +7,10 @@ import android.text.TextWatcher;
 import android.util.Log;
 import android.view.KeyEvent;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.view.View.OnKeyListener;
+import android.view.View.OnTouchListener;
 import android.view.ViewGroup;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
@@ -18,9 +19,10 @@ import android.widget.TextView.OnEditorActionListener;
 
 import com.actionbarsherlock.app.SherlockFragment;
 
-public class HackerTyperFragment extends SherlockFragment implements OnKeyListener {
+public class HackerTyperFragment extends SherlockFragment {
 	
-	private EditText mHackerView;
+	private EditText mHackerViewHidden;
+	private TextView mHackerView;
 	
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -31,33 +33,37 @@ public class HackerTyperFragment extends SherlockFragment implements OnKeyListen
 	@Override
 	public void onViewCreated(View view, Bundle savedInstanceState) {
 		super.onViewCreated(view, savedInstanceState);
-		mHackerView = (EditText) view.findViewById(R.id.hacker_typer_et);
+		mHackerView = (TextView) view.findViewById(R.id.hacker_typer_tv);
+		mHackerViewHidden = (EditText) view.findViewById(R.id.hacker_et);
 		
-		/*mHackerView.setOnClickListener(new OnClickListener() {
-			
+		mHackerView.setOnTouchListener(new OnTouchListener() {
+			 
 			@Override
-			public void onClick(View v) {
-				InputMethodManager inputMethodManager=(InputMethodManager)getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
-				inputMethodManager.toggleSoftInputFromWindow(mHackerView.getApplicationWindowToken(), InputMethodManager.SHOW_FORCED, 0);
-			}
-		});*/
-		
-		mHackerView.setOnEditorActionListener(new OnEditorActionListener() {
-			
-			@Override
-			public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
-				Log.e("p37td8" , "onEditorAction");
-				
+			public boolean onTouch(View v, MotionEvent event) {
+				mHackerViewHidden.dispatchTouchEvent(event);
 				return false;
 			}
 		});
 		
-		mHackerView.setOnKeyListener(this);
-		mHackerView.addTextChangedListener(new TextWatcher() {
+		mHackerViewHidden.setOnTouchListener(new OnTouchListener() {
+			
+			@Override
+			public boolean onTouch(View v, MotionEvent event) {
+				if(event.getAction() == MotionEvent.ACTION_DOWN){
+					InputMethodManager imm = (InputMethodManager) getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
+//					imm.showSoftInput(v, InputMethodManager.SHOW_IMPLICIT);
+					imm.toggleSoftInputFromWindow(v.getApplicationWindowToken(), InputMethodManager.SHOW_FORCED, 0); //TODO
+				}
+				return false;
+			}
+		});
+
+		mHackerViewHidden.addTextChangedListener(new TextWatcher() {
 			
 			@Override
 			public void onTextChanged(CharSequence s, int start, int before, int count) {
 				Log.e("p37td8" , "onTextChanged");
+				mHackerView.setText(mHackerView.getText() + " 11 ");
 			}
 			
 			@Override
@@ -68,18 +74,8 @@ public class HackerTyperFragment extends SherlockFragment implements OnKeyListen
 			
 			@Override
 			public void afterTextChanged(Editable s) {
-				s.clear();
-				mHackerView.add
-				mHackerView.setText(" 111 ");
-				
+				//EMPTY
 			}
 		});
 	}
-
-	@Override
-	public boolean onKey(View v, int keyCode, KeyEvent event) {
-		Log.e("p37td8" , "onKey" + keyCode);
-		return false;
-	}
-
 }
